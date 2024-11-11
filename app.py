fb1c915ea8ccb49d0692f11aeec290f406c562dc
belug <andrewyu7087@gmail.com>
2024-11-11 16:43:13 -0500

    Commit 21 - Changing tab bar

--- app.py
+++ app.py
@@ -7,33 +7,65 @@ import os
 from dotenv import load_dotenv, dotenv_values
 
 
+
+st.set_page_config(page_title="BA Group AI Assistant", page_icon="logo.jpg", layout="wide")
 load_dotenv()
-
-# Check if running in Streamlit's production environment (e.g., on Streamlit Cloud)
-if "API_KEY" in st.secrets:
+try:
+    # Try to get the API key from Streamlit secrets
     api_url = st.secrets["API_KEY"]
-else:
-
+except (KeyError, FileNotFoundError):
+    # If there's a KeyError or FileNotFoundError (no `secrets.toml`), fallback to environment variable
     api_url = os.getenv("MY_SECRET_KEY")
-
-st.set_page_config(page_title="BA Group LLM", page_icon="🧠", layout="wide")
 
 # Initialize session state for chat history
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
         {"role": "AI", "content": "Hello, I am a bot. How can I help you?"},
     ]
-
+#ds
 def clear_screen():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 with st.sidebar:
-    st.title("BA Group LLM Assistant")
+    st.image("bagroup_logo.png")
+    st.markdown("This is a preliminary test version of a BA Group AI Assistant tool")
     st.button('Clear Screen', on_click=clear_screen)
 
-    chunk_type = st.radio("Chunking Strategy", ["Semantic chunking", "Hierarchical chunking", "Semantic Chunking With Cohere"], index=0)
+    # chunk_type = st.radio("Chunking Strategy", ["Semantic Chunking With Cohere", "Hierarchal Chunking With Cohere", "Semantic Chunking With Cohere OpenSearch", "Semantic Chunking With Cohere OpenSearch & Small to Big Retrieval"], index=0)
+    # model_type = st.radio("Foundation Model",
+    #                       ["Claude 3.5 Sonnet v2", "Cohere Command R"], index=0)
+    # conversation_toggle = st.toggle("Conversational Memory")
+    #
+    # num_results = st.number_input(
+    #     "Number of Sources", value=5, placeholder="Type a number", max_value=10, min_value=1
+    # )
+    st.header("1. Informational Purposes Only")
+
+    st.markdown("This app is designed to help assist consultants with general queries. The information generated may not always be accurate, complete, or up-to-date. It is recommended to critically review information before making decisions based on the AI's responses.")
 
 
+    st.header("2. Data Sources and Limitations")
+
+    st.markdown("This AI system relies on a knowledgebase that may have limitations in terms of scope and accuracy. Due to limitations in data sources, the AI may not be able to provide a comprehensive response to every query")
+
+
+
+
+#hard coded parameters
+chunk_type = "Semantic Chunking With Cohere"
+model_type = "Claude 3.5 Sonnet v2"
+
+num_results = 8
+conversation_toggle = False
+
+
+
+
+
+
+
+
+#ds
 
 def get_conversation_history():
     """
@@ -58,7 +90,7 @@ def typewriter_effect(text, speed=0.05):
 
 
 # Function to call the API
-def call_api(chat_history, prompt, chunk_type):
+def call_api(chat_history, prompt, chunk_type, model_type, num_results):
     # Replace with your actual API endpoint
 
 
@@ -67,8 +99,22 @@ def call_api(chat_history, prompt, chunk_type):
                            "chunk_type": chunk_type
                            }
 
+    if conversation_toggle:
+        prompt_with_history = {"conversation": chat_history,
+                               "prompt": prompt,
+                               "chunk_type": chunk_type,
+                               "model": model_type,
+                               "num_results": num_results
+                               }
 
 
+    else:
+        prompt_with_history = {"conversation": "",
+                               "prompt": prompt,
+                               "chunk_type": chunk_type,
+                               "model": model_type,
+                               "num_results": num_results
+                               }
     try:
         response = requests.post(api_url, json=prompt_with_history)
         response.raise_for_status()  # Raise an exception for bad status codes
@@ -112,12 +158,7 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("Human"):
         st.write(prompt)
 
-
-
-
-
-
-    result = call_api(conversation_history, prompt, chunk_type)
+    result = call_api(conversation_history, prompt, chunk_type, model_type, num_results)
 
     assistant_response = result.get('generated_responses')
 
@@ -130,8 +171,8 @@ if prompt := st.chat_input("What is up?"):
             st.json({
                 # "Reference": result.get('referenced_document', conversation_history),
                 "Referenced Document": result.get('referenced_document'),
-                "Status Code": result.get('statusCode', 'N/A'),
-                "Source": result.get('prompt', 'N/A')
+                "Status Code": result.get('statusCode'),
+                "Source": result.get('file_location', 'N/A')
                 })
 
 
